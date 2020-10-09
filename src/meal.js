2f61601791156aeae998e5079d6c9a27d88272b0
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-10-08 21:30:25 -0600

    Add Meal.js - Test 17 passing (15ms)

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,40 @@
-function nameMenuItem() {
+function nameMenuItem(menuItemName) {
+  return `Delicious ${menuItemName}`;
+};
 
+function createMenuItem(name, price, type) {
+    var menuItem = {
+    name: name,
+    price: price,
+    type: type,
+  };
+  return menuItem;
+};
+
+function addIngredients(food, ingredients) {
+  if (ingredients.includes(food) !== true) {
+  ingredients.push(food);
+} else {
+  return ingredients;
 }
+};
 
+function formatPrice(initialPrice) {
+  return "$" + initialPrice;
+};
 
+function decreasePrice(initialPrice) {
+  return initialPrice * .9
+};
+
+function createRecipe(title, ingredients, menuItemType) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType,
+  }
+  return recipe;
+};
 module.exports = {
-  nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  nameMenuItem, createMenuItem, addIngredients, formatPrice, decreasePrice, createRecipe
 }
-
-
