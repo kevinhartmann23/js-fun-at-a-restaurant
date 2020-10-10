6af2d5babd75f88c96fa892ed0dce26cee907e1c
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-10-10 15:08:00 -0600

    Add order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,31 @@
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3){
+    deliveryOrders.push(order);
+  };
+};
 
-module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
-}
+function refundOrder (orderNumber, deliveryOrders) {
+  return deliveryOrders.splice(orderNumber - 1, 1);
+};
+
+function listItems(deliveryOrders, items) {
+  var list = [];
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    items = deliveryOrders[i].item;
+    list.push(items);
+    };
+  return list.join(", ");
+};
+
+function searchOrder(deliveryOrders, orderSearch) {
+  var result = false
+  for (var i = 0; i < deliveryOrders.length; i++) {
+    if (orderSearch === deliveryOrders[i].item) {
+      return result = true;
+    }
+    }
+  return result;
+};
+
+
+module.exports = {takeOrder, refundOrder, listItems, searchOrder}
