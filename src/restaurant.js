901c63f68985f4246dc0dd4261833fbad1456355
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-10-10 17:10:19 -0600

    Pass test line 129

--- src/restaurant.js
+++ src/restaurant.js
@@ -11,8 +11,20 @@ var pizzaRestaurant = {
   return pizzaRestaurant;
 };
 
-function addMenuItem() {
-
+function addMenuItem(pizzaRestaurant, menuItem) {
+    if (menuItem.type === "lunch") {
+      return pizzaRestaurant.menus.lunch.push(menuItem);
+    } else if (menuItem.type === "breakfast") {
+      return pizzaRestaurant.menus.breakfast.push(menuItem);
+    } else {
+      return pizzaRestaurant.menus.dinner.push(menuItem);
+    };
+    // if (pizzaRestaurant.menus.name.includes(menuItem) !== true) {
+    //   pizzaRestaurant.menus.name.push(menuItem);
+    // } else {
+    //   return menuItem;
+    // }
+    return menuItem;
 };
 
 module.exports = {createRestaurant, addMenuItem,
