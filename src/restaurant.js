10d3017c98a19b90c2a046e58e7ad2c14deaa76d
Kevin Hartmann <kevinhartmann23@gmail.com>
2020-10-11 12:34:54 -0600

    Added functions - not complete

--- src/restaurant.js
+++ src/restaurant.js
@@ -12,21 +12,35 @@ var pizzaRestaurant = {
 };
 
 function addMenuItem(pizzaRestaurant, menuItem) {
-    if (menuItem.type === "lunch") {
+  if(menuItem.type === "lunch") {
       return pizzaRestaurant.menus.lunch.push(menuItem);
     } else if (menuItem.type === "breakfast") {
       return pizzaRestaurant.menus.breakfast.push(menuItem);
+    } else if (menuItem.type === "dinner"){
+      return pizzaRestaurant.menus.dinner.push(menuItem)
     } else {
-      return pizzaRestaurant.menus.dinner.push(menuItem);
+      return ;
     };
-    // if (pizzaRestaurant.menus.name.includes(menuItem) !== true) {
-    //   pizzaRestaurant.menus.name.push(menuItem);
+
+    ////pizzaRestaurant.menus.breakfast.pop();
+    // if (pizzaRestaurant.includes(menuItem) !== true) {
+    //   pizzaRestaurant.push(menuItem);
     // } else {
-    //   return menuItem;
+    //   return pizzaRestaurant;
     // }
-    return menuItem;
 };
 
-module.exports = {createRestaurant, addMenuItem,
-  // removeMenuItem
+function removeMenuItem(pizzaRestaurant, menuItem) {
+  if(menuItem === menuItem.name) {
+      return pizzaRestaurant.menus.pop(menuItem);
+    } else {
+      return ;
+    }
+// if (menuItem.name === menuItem)
+//   return pizzaRestaurant.menus.splice(pizzaRestaurant.menus.menuItem);
+};
+
+//I feel like im close, but
+
+module.exports = {createRestaurant, addMenuItem, removeMenuItem
 }
